5e6a5577551c91c8010f7c205615717a0517cee3
Asim 3 <asimwebapps@gmail.com>
2024-11-04 21:42:15 +0300

    Component_3 = useState

--- template-src/components/Component_3/index.tsx
+++ template-src/components/Component_3/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./styles"
 import { View, Text, Button } from 'react-native';
 import { Link } from 'expo-router';
 
 
 export default function Component() {
+  const [MyCount, setMyCount] = useState(99);
+
   return (
     <View style={styles.scroll_view}>
-      <Link href={'/modal'} asChild>
-        <Button title='Component 3' />
-      </Link>
+      <Text>MyCount = {MyCount}</Text>
+      <Button title='Component 3.1: MyCount - 1' onPress={() => setMyCount(MyCount - 1)} />
+      <Button title='Component 3.2: MyCount + 1' onPress={() => setMyCount(MyCount + 1)} />
     </View>
   );
 };
